f0e864d1ee2b1f0279662b0bd28246a54f1c58d6
Fernando Vinicios Manchini de Souza <fernandomanchini@gmail.com>
2018-03-26 06:55:13 -0300

    Promini traffic counter
    commit promini traffic counter

---
+++ promini8-traffic-counter/src/config.h
@@ -0,0 +1,5 @@
+static const u1_t PROGMEM APPEUI[8]={  };   // Chose LSB mode on the console and then copy it here.
+
+static const u1_t PROGMEM DEVEUI[8]={  };   // LSB mode
+
+static const u1_t PROGMEM APPKEY[16] = {  }; // MSB mode
